621c938eb1da552a4082631d930e5ec7a49cc3bb
Cristian Riaño <cristianriano@unicauca.edu.co>
2015-09-18 17:12:34 -0500

    acciones crud implementadas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :admin_user,  only: [:destroy, :create, :new, :edit, :update]
+  before_action :admin_user,  only: [:destroy, :create, :new, :edit, :update, :index]
+
+  def index
+  	@users = User.paginate(page: params[:page])
+  end
 
   def new
   	@user = User.new
@@ -9,7 +13,7 @@ class UsersController < ApplicationController
 	@user = User.new(user_params)
 	if @user.save
 		flash[:info] = "Usuario creado"
-		redirect_to current_user
+		redirect_to users_path
 	else
 		render 'new'
 	end
@@ -19,16 +23,26 @@ class UsersController < ApplicationController
 	@user = User.find(params[:id])
   end
 
+  def destroy
+  	User.find(params[:id]).destroy
+    flash[:success] = "Usuario eliminado"
+    redirect_to users_url
+  end
+
   private
 
 	  #Evita la inyteccion de parametros indeseados por medio de un proxy
 	  def user_params
-	  	params.require(:user).permit(:name, :email, :document)
+	  	params.require(:user).permit(:name, :password, :password_confirmation, :zone)
 	  end
 
 	  #Verifica que el usuario tenga privilegios administrativos
 	  def admin_user
-	  	redirect_to(current_user) unless current_user.admin?
+	  	if current_user.nil?
+	  		redirect_to(login_path)
+	  	else
+	  		redirect_to(current_user) unless current_user.admin?
+	  	end
 	  end
 
 	  #Verifica que el usuario haya logueado
