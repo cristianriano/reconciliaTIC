1fd8c0901e52ce57d0ea41d97e8aa70a6263e0d7
Cristian Riaño <cristianriano@unicauca.edu.co>
2015-09-19 03:05:33 -0500

    Implementado el sistema de preguntas y respuestas

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,16 @@
 class AnswersController < ApplicationController
   def new
+  	@answer = Answer.new()
+  end
+
+  def create
+  	@answer = Answer.new(question_id: params[:id], content: params[:answer][:content])
+  	if @answer.save
+  		flash[:success] = "Pregunta contestada. Gracias"
+  		redirect_to questions_user_path(current_user)
+  	else
+  		flash[:danger] = "Error"
+  		redirect_to new_answer_path(id: params.id)
+  	end
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,5 +5,6 @@ class QuestionsController < ApplicationController
   end
 
   def show
+  	@question = Question.find(params[:id])
   end
 end
